3ddf6143457983ecbd2e79c197b0d1ce4a0b1b21
jerimy321 <123806723+jerimy321@users.noreply.github.com>
2024-05-20 18:03:54 +0900

    create: router 설치 / main페이지 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/main';
+import Home from './pages/home';
 
 function App() {
   return (
-    <>
-      <Main />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/main" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
