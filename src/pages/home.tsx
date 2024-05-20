fb59f16b2bc3248d674d0d06a31b5ae4f0e3027b
insidesjsj <tjdwns428@naver.com>
2024-05-21 02:59:44 +0900

    modify: 네이버 로그인 토큰 전역 상태로 저장

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,20 @@
 import {Link} from 'react-router-dom';
+import {useRecoilValue} from 'recoil';
+import {naverTokenState} from '../recoil/recoilState';
 
 export default function Home() {
+    const token = useRecoilValue(naverTokenState);
+
     return (
         <div>
-            <></>
+            <div>홈</div>
             {/* 로그인 기능 구현을 위해 임시적으로 만듦 */}
-            <Link to="/login">로그인</Link>
+            {token !== null ?
+                <div>로그인이 완료 되었습니다.</div>
+                : (
+                    <Link to="/login">로그인</Link>
+                )
+            }
         </div>
     );
-}
+};
--- src/pages/naverCallback.tsx
+++ src/pages/naverCallback.tsx
@@ -1,10 +1,21 @@
 import {useEffect} from 'react';
+import {useLocation} from 'react-router-dom';
+import {useSetRecoilState} from 'recoil';
+import {naverTokenState} from '../recoil/recoilState';
 
 export default function NaverCallback() {
+    const location = useLocation();
+    const setTokenState = useSetRecoilState(naverTokenState);
 
     useEffect(() => {
+        const hash = location.hash;
+        const params = new URLSearchParams(hash.substring(1));
+        const token = params.get('access_token');
+        if (token) {
+            setTokenState(token);
             window.location.href = '/';
-    }, []);
+        }
+    }, [location]);
 
     return (
         <div>
