069ada29269800628f696a07123bc8ee2c2be82b
insidesjsj <tjdwns428@naver.com>
2024-05-22 04:14:55 +0900

    modify: 로그인 화면 배경설정 등 CSS 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,14 +9,22 @@ module.exports = {
       fontSize: {
         'home-div': ['78px', { lineHeight: '126.3%', letterSpacing: '-1.56px' }],
         'home-button': ['24px', { lineHeight: '137%', letterSpacing: '-0.48px' }],
+        'login-need': ['42px', { lineHeight: '140%', letterSpacing: '-2px' }],
+        'login-simple': ['24px', { lineHeight: '140%', letterSpacing: '-2px' }],
       },
       width: {
         '972': '972px',
+        '709': '709px',
+        '464': '464px',
         '384': '384px',
       },
       height: {
         '611': '611px',
+        '433': '433px',
         '90': '90px',
+      },
+      margin: {
+        '75': '75px',
       },
       padding: {
         '67': '67px',
@@ -24,11 +32,17 @@ module.exports = {
       backgroundImage: {
         'home-bg': "url('./assets/images/bg_main.svg')",
         'home-button': 'linear-gradient(92deg, rgba(255, 255, 255, 0.30) 0.48%, rgba(255, 255, 255, 0.60) 99.34%)',
+        'login-bg': "url('./assets/images/bg_login.png')",
       },
       boxShadow: {
         'home-button':'0px 0px 30px 4px rgba(219, 212, 223, 0.50)',
         'home-button-hover':'0px 0px 30px 6px rgba(0, 0, 0, 0.50)',
-      }
+        'login-modal': '0px 0px 100px 12px rgba(0, 0, 0, 0.40)',
+      },
+      borderRadius: {
+        '30': '30px',
+      },
+
     },
   },
   plugins: [],
