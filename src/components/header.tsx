e9d73993ca030682bb011e5abd49070f70dc1d6d
jerimy321 <123806723+jerimy321@users.noreply.github.com>
2024-05-23 16:19:18 +0900

    modify: header css 및 메인,마이페이지 연결 / loginout 추가 필요

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,21 +2,34 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className="fixed top-0 z-10 px-10 py-3 w-dvw font-pretendard">
+    <header className="fixed top-0 z-10 px-10 py-3 w-dvw font-pretendard h-[100px]">
       <nav>
-        <ul className="flex justify-between space-x-4">
+        <ul className="flex justify-between px-10">
           <li>
-            <Link to="/" className="text-black no-underline">
+            <Link
+              to="/"
+              className="no-underline text-header-default text-[24px]"
+            >
               로고
             </Link>
           </li>
           <li className="flex flex-row gap-4">
             <li>
-              <Link to="/mypage" className="text-black no-underline">
+              <Link
+                to="/mypage"
+                className="no-underline text-header-default text-[24px] font-normal"
+              >
                 MY PAGE
               </Link>
             </li>
-            <li>LOGOUT</li>
+            <li>
+              <Link
+                to="/"
+                className="no-underline text-header-default text-[24px] font-normal"
+              >
+                LOGOUT
+              </Link>
+            </li>
           </li>
         </ul>
       </nav>
