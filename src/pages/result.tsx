6f186d2049170ded3990ea8b58389037e205ad0c
insidesjsj <tjdwns428@naver.com>
2024-05-24 04:54:32 +0900

    modify: 비슷한 향수 아이템 하버 이벤트

--- src/pages/result.tsx
+++ src/pages/result.tsx
@@ -26,44 +26,56 @@ export default function Result() {
     };
 
     const clickSubPerfume = (data: resultPerfumeData) => () => {
-        navigate(`/perfumeInfo/${data.id}`, { state: { perfume: data } });
+        navigate(`/perfumeInfo/${data.id}`, {state: {perfume: data}});
     }
-
     return (
         <div className='w-screen h-screen'>
-
-            {/* 메인 제품 */}
-            <div className='w-1/2 mx-auto'>
-                <div className='text-center'>당신의 취향을 저격할 향수</div>
-                <div className='flex justify-between w-full'>
-                    <div>
-                        <div>{mainPerMockData.brand}</div>
-                        <div>{mainPerMockData.name}</div>
-                        <div>{mainPerMockData.engName}</div>
-                        <div className='flex' onClick={SaveClick}><img src={save}/>내 향수 저장하기</div>
-                    </div>
-                    <div>
-                        <img src={mainPerMockData.imageURL}/>
+            <div className='h-full'>
+                {/* 메인 제품 */}
+                <div className='w-1/2 mx-auto'>
+                    <div className='text-center'>당신의 취향을 저격할 향수</div>
+                    <div className='flex justify-between w-full'>
+                        <div>
+                            <div>{mainPerMockData.brand}</div>
+                            <div>{mainPerMockData.name}</div>
+                            <div>{mainPerMockData.engName}</div>
+                            <div className='flex' onClick={SaveClick}><img src={save}/>내 향수 저장하기</div>
+                        </div>
+                        <div>
+                            <img src={mainPerMockData.imageURL}/>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            {/* 비슷한 제품*/}
-            <div>
-                <div>내 향수와 비슷한 제품들이에요</div>
+                {/* 비슷한 제품*/}
                 <div>
-                    <div className='flex'>
-                        <button onClick={prevClick} disabled={currentPage === 0}>{'<'}</button>
-                        {subPerMockData
-                            .slice(currentPage, currentPage + subPerfumePerPage)
-                            .map((data) => (
-                                <div key={data.id} onClick={clickSubPerfume(data)}>
-                                    <img src={data.imageURL} alt={data.name}/>
-                                </div>
-                            ))}
-                        <button onClick={nextClick} disabled={currentPage >= subPerMockData.length - 3}>{'>'}</button>
-                    </div>
+                    <div>내 향수와 비슷한 제품들이에요</div>
                     <div>
+                        <div className='flex'>
+                            <button onClick={prevClick} disabled={currentPage === 0}>{'<'}</button>
+                            {subPerMockData
+                                .slice(currentPage, currentPage + subPerfumePerPage)
+                                .map((data) => (
+                                    <div key={data.id} onClick={clickSubPerfume(data)}
+                                         className='relative group mx-2 z-1'>
+                                        <img src={data.imageURL} alt={data.name}/>
+                                        <div
+                                            className='absolute inset-0 hidden justify-center group-hover:flex group-hover:bg-black group-hover:bg-opacity-40'>
+                                            <div className='flex flex-col text-white items-center justify-center'>
+                                                <span>{data.brand}</span>
+                                                <span>{data.name}</span>
+                                                <span>{data.engName}</span>
+                                                <div onClick={SaveClick} className='z-0'>
+                                                    <img src={save} className='cursor-pointer opacity-100'
+                                                         onClick={SaveClick} />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            <button onClick={nextClick}
+                                    disabled={currentPage >= subPerMockData.length - 3}>{'>'}</button>
+                        </div>
                     </div>
                 </div>
             </div>
