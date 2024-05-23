a4994c5a0be347c285f2ce5da95aed3c1dc14d4b
insidesjsj <tjdwns428@naver.com>
2024-05-24 01:19:49 +0900

    modify: 결과 페이지 페이지네이션 기능 추가

--- src/pages/result.tsx
+++ src/pages/result.tsx
@@ -6,6 +6,15 @@ import SaveAlert from '../components/saveAlert';
 const subPerfumePerPage = 3;
 export default function Result() {
     const [saveComplete, setSaveComplete] = useState(false);
+    const [currentPage, setCurrentPage] = useState(0);
+
+    const prevClick = () => {
+        setCurrentPage((prevPage) => Math.max(prevPage - 1, 0));
+    };
+
+    const nextClick = () => {
+        setCurrentPage((prevPage) => Math.min(prevPage + 1, subPerMockData.length - 3));
+    };
 
     const SaveClick = () => {
         setSaveComplete(true);
@@ -25,10 +34,10 @@ export default function Result() {
                         <div>{mainPerMockData.brand}</div>
                         <div>{mainPerMockData.korName}</div>
                         <div>{mainPerMockData.engName}</div>
-                        <div className='flex' onClick={SaveClick}><img src={save} />내 향수 저장하기</div>
+                        <div className='flex' onClick={SaveClick}><img src={save}/>내 향수 저장하기</div>
                     </div>
                     <div>
-                        <img src={mainPerMockData.img} />
+                        <img src={mainPerMockData.img}/>
                     </div>
                 </div>
             </div>
@@ -38,18 +47,23 @@ export default function Result() {
                 <div>내 향수와 비슷한 제품들이에요</div>
                 <div>
                     <div className='flex'>
+                        <button onClick={prevClick} disabled={currentPage === 0}>{'<'}</button>
                         {subPerMockData
+                            .slice(currentPage, currentPage + subPerfumePerPage)
                             .map((v) => (
                                 <div key={v.id}>
-                                    <img src={v.img} alt={v.kr_name} />
+                                    <img src={v.img} alt={v.kr_name}/>
                                 </div>
                             ))}
+                        <button onClick={nextClick} disabled={currentPage >= subPerMockData.length - 3}>{'>'}</button>
+                    </div>
+                    <div>
                     </div>
                 </div>
             </div>
             {/* 저장 알림 모달 */}
             {saveComplete && (
-                <SaveAlert />
+                <SaveAlert/>
             )}
         </div>
     )
