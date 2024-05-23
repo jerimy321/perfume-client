a62e184fb5dad0f79f178c4704720aeee401ff3a
insidesjsj <tjdwns428@naver.com>
2024-05-24 01:01:34 +0900

    create: result 페이지에서 쓸 향수 Mock 데이터 생성

---
+++ src/data/resultPerfumeData.ts
@@ -0,0 +1,54 @@
+import mainPerfume from '../assets/images/mainPerfume.png';
+import subPerfume from '../assets/images/subPerfume.png';
+
+export const mainPerMockData = {
+    id: '01',
+    brand: '조말론',
+    korName: '라임 바질 만다린',
+    engName: 'Lime Basil & Mandarin',
+    content: '아주 멋진 향수입니다.',
+    img: mainPerfume,
+}
+
+export const subPerMockData = [
+    {
+        id: '01',
+        brand: '딥 다크',
+        kr_name: '로 드 타로코',
+        en_name: 'L`EAU de Tarocco',
+        content: '아주 멋진 향수입니다.',
+        img: subPerfume,
+    },
+    {
+        id: '02',
+        brand: '딥 다크',
+        kr_name: '로 드 타로코',
+        en_name: 'L`EAU de Tarocco',
+        content: '아주 멋진 향수입니다.',
+        img: subPerfume,
+    },
+    {
+        id: '03',
+        brand: '딥 다크',
+        kr_name: '로 드 타로코',
+        en_name: 'L`EAU de Tarocco',
+        content: '아주 멋진 향수입니다.',
+        img: subPerfume,
+    },
+    {
+        id: '04',
+        brand: '딥 다크',
+        kr_name: '로 드 타로코',
+        en_name: 'L`EAU de Tarocco',
+        content: '아주 멋진 향수입니다.',
+        img: subPerfume,
+    },
+    {
+        id: '05',
+        brand: '딥 다크',
+        kr_name: '로 드 타로코',
+        en_name: 'L`EAU de Tarocco',
+        content: '아주 멋진 향수입니다.',
+        img: subPerfume,
+    },
+];
