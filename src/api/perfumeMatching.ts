7d75ea89437672d490c8a83af11e299cf34a21e6
jerimy321 <123806723+jerimy321@users.noreply.github.com>
2024-05-23 17:44:38 +0900

    create: api / 서버통신 테스트

---
+++ src/api/perfumeMatching.ts
@@ -0,0 +1,11 @@
+import axiosInstance from './axiosConfig';
+
+export const postHashtags = async (hashtags: string[]) => {
+  try {
+    const response = await axiosInstance.post('/recommend', { hashtags });
+    return response.data;
+  } catch (error) {
+    console.error('Error posting hashtags:', error);
+    throw error;
+  }
+};
