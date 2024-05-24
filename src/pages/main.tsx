feb7fc023b4e7dbac27a145952f7845a7a616c53
jerimy321 <123806723+jerimy321@users.noreply.github.com>
2024-05-24 23:57:06 +0900

    css: mainpage css

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -10,10 +10,8 @@ export default function Main() {
   console.log('Hashtag List:', hashtagList);
 
   return (
-    <div className="flex flex-col items-center justify-between w-screen h-screen bg-center bg-cover bg-home-bg font-pretendard">
-      <div className="flex flex-col items-center justify-center w-full">
-        <Carousel />
-      </div>
+    <div className="flex flex-col items-center justify-between flex-1 w-screen h-screen bg-center bg-cover bg-home-bg font-pretendard">
+      <Carousel />
     </div>
   );
 }
