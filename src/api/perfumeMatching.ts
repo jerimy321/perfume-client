bd74abdb8f2c4a869959af299df35f761be5f9c4
jerimy321 <123806723+jerimy321@users.noreply.github.com>
2024-05-24 12:01:19 +0900

    api url 수정

--- src/api/perfumeMatching.ts
+++ src/api/perfumeMatching.ts
@@ -2,7 +2,7 @@ import axiosInstance from './axiosConfig';
 
 export const postHashtags = async (hashtags: string[]) => {
   try {
-    const response = await axiosInstance.post('/recommend', { hashtags });
+    const response = await axiosInstance.post('recommend', { hashtags });
     return response.data;
   } catch (error) {
     console.error('Error posting hashtags:', error);
