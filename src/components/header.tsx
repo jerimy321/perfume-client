870e851332c9155e83f0615689aa36d3b3e96e13
jerimy321 <123806723+jerimy321@users.noreply.github.com>
2024-05-25 14:40:31 +0900

    header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -23,47 +23,52 @@ const Header = () => {
   }
 
   return (
-    <header className="fixed top-0 z-10 w-[1920px] m-0 p-0 font-pretendard h-[100px]">
-      <ul className="flex items-center justify-between p-0 m-0">
-        <li>
-          <Link to="/" className="no-underline text-header-default">
-            <img src="/logo.png" alt="Logo" className="w-[130px] ml-[78px]" />
-          </Link>
-        </li>
-        <ul className="flex flex-row">
-          {naverToken && (
-            <li>
-              <Link
-                to="/mypage"
-                className="no-underline my-[38px] text-header-default text-[20px] font-normal"
-              >
-                MY PAGE
-              </Link>
-            </li>
-          )}
-          {naverToken ? (
-            <li>
-              <button
-                onClick={handleLogout}
-                className="no-underline mr-[90px] my-[38px] text-header-default text-[20px] font-normal bg-transparent border-none cursor-pointer"
-              >
-                LOGOUT
-              </button>
-            </li>
-          ) : (
-            <li>
-              <button
-                data-toggle="modal"
-                data-target="#loginModal"
-                onClick={handleLogin}
-                className="no-underline my-[38px] mr-[90px] text-header-default text-[20px] font-normal bg-transparent border-none cursor-pointer"
-              >
-                LOGIN
-              </button>
-            </li>
-          )}
+    <header className="fixed top-0 z-10 px-10 w-dvw font-pretendard h-[100px]">
+      <nav>
+        <ul className="flex items-center justify-between px-10">
+          <li>
+            <Link
+              to="/"
+              className="no-underline text-header-default text-[20px]"
+            >
+              <img src="/logo.png" alt="Logo" className="h-[67px]" />
+            </Link>
+          </li>
+          <ul className="flex flex-row gap-4">
+            {naverToken && (
+              <li>
+                <Link
+                  to="/mypage"
+                  className="no-underline text-header-default text-[20px] font-normal"
+                >
+                  MY PAGE
+                </Link>
+              </li>
+            )}
+            {naverToken ? (
+              <li>
+                <button
+                  onClick={handleLogout}
+                  className="no-underline text-header-default text-[20px] font-normal bg-transparent border-none cursor-pointer"
+                >
+                  LOGOUT
+                </button>
+              </li>
+            ) : (
+              <li>
+                <button
+                  data-toggle="modal"
+                  data-target="#loginModal"
+                  onClick={handleLogin}
+                  className="no-underline text-header-default text-[20px] font-normal bg-transparent border-none cursor-pointer"
+                >
+                  LOGIN
+                </button>
+              </li>
+            )}
+          </ul>
         </ul>
-      </ul>
+      </nav>
     </header>
   );
 };
