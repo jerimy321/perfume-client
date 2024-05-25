d8b30469123a63ea78f49a14b8bf2bc48fa65828
jerimy321 <123806723+jerimy321@users.noreply.github.com>
2024-05-25 11:24:07 +0900

    css: matching page

--- src/pages/matchfail.tsx
+++ src/pages/matchfail.tsx
@@ -6,19 +6,20 @@ export default function MatchFail() {
     nav('/main');
   };
   return (
-    <div className="flex flex-col items-center w-screen h-screen bg-center bg-cover bg-matching-none-bg font-pretendard">
-      <span className="text-[68px] font-normal mt-20">아쉬워요,</span>
-      <span className="text-[68px] font-normal">
+    <div className="flex flex-col items-center w-[1920px] h-[1080px] bg-center bg-cover bg-matching-none-bg font-pretendard">
+      <span className="text-pick-title font-normal mt-[99px]">아쉬워요,</span>
+      <span className="font-normal text-pick-title">
         <span className="font-semibold">취향 저격 향수</span>를 찾을 수 없어요.
       </span>
 
-      <span className="text-[42px] font-normal mt-[100px] mb-20  text-mainbutton-default">
-        취향을 수정해 다시 한 번 시도해볼까요?
+      <span className="text-pick-subtitle font-normal mt-[123px] mb-[68px] text-mainbutton-default">
+        <span className="font-semibold">취향을 수정</span>해 다시 한 번
+        <span className="font-semibold">시도</span>해볼까요?
       </span>
 
       <button
-        onClick={handleClicked}
         className="flex w-384 h-90 py-2 px-67 justify-center items-center gap-2.5 mx-auto rounded-2xl border-2 border-white font-pretendard text-home-button bg-home-button shadow-home-button hover:bg-white hover:shadow-home-button-hover"
+        onClick={handleClicked}
       >
         추천 받으러 가기
       </button>
