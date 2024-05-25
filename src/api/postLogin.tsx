59661af4a622e1aecfaf10b827dda620ef29f46f
insidesjsj <tjdwns428@naver.com>
2024-05-25 18:52:34 +0900

    로그인 경로 수정

--- src/api/postLogin.tsx
+++ src/api/postLogin.tsx
@@ -2,9 +2,14 @@ import axiosInstance from './axiosConfig';
 
 export const postLogin = async () => {
     try {
-        const response = await axiosInstance.post('login');
+        const response = await axiosInstance.post('oauth2/authorization/naver');
         console.log(response.data);
+        console.log(response.data.redirectUrl);
+        if (response.data.redirectUrl) {
+            window.location.href = response.data.redirectUrl;
+        }
         return response.data;
+
     } catch (error) {
         console.error('Error posting hashtags:', error);
         throw error;
