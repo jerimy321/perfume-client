0298ec518eac79865f43ce21566f35d41489a020
insidesjsj <tjdwns428@naver.com>
2024-05-25 19:21:08 +0900

    로그인 경로 수정2

--- src/api/postLogin.tsx
+++ src/api/postLogin.tsx
@@ -2,9 +2,9 @@ import axiosInstance from './axiosConfig';
 
 export const postLogin = async () => {
     try {
-        const response = await axiosInstance.post('login');
-        console.log(response.data);
-        console.log(response.data.redirectUrl);
+        const response = await axiosInstance.post('/oauth2/authorization/naver');
+        console.log('아니면 이게 url?' +response.data);
+        console.log('이게 url?' + response.data.redirectUrl);
         if (response.data.redirectUrl) {
             window.location.href = response.data.redirectUrl;
         }
