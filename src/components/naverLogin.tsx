e04a8cd09fa0bc42f8ce842ca73d8277ca456bcc
jerimy321 <123806723+jerimy321@users.noreply.github.com>
2024-05-26 13:34:20 +0900

    login

--- src/components/naverLogin.tsx
+++ src/components/naverLogin.tsx
@@ -4,15 +4,17 @@ import naverHover from '../assets/images/logo_white.png';
 import axios from 'axios';
 
 const NaverLogin = () => {
-    const [isHover, setIsHover] = useState(false);
-    const loginNaver = async () => {
-        try {
-            const response = await axios.post(process.env.REACT_APP_API_URL + 'oauth2/authorization/naver');
-            console.log(response.data)
-        } catch (error) {
-            console.error('Error Posting Login')
-        }
-    };
+  const [isHover, setIsHover] = useState(false);
+  const loginNaver = async () => {
+    try {
+      const response = await axios.get(
+        process.env.REACT_APP_API_URL + 'oauth2/authorization/naver',
+      );
+      window.location.href = response.data;
+    } catch (error) {
+      console.error('Error Posting Login');
+    }
+  };
 
   return (
     <div className={'flex justify-center items-center mb-[70px]'}>
