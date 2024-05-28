bb7623420c590125441b60ad846c8acdc140dfbc
insidesjsj <tjdwns428@naver.com>
2024-05-28 16:57:24 +0900

    프록시 수정

--- src/setUpProxy.ts
+++ src/setUpProxy.ts
@@ -4,7 +4,7 @@ module.exports = function (app: any) {
     app.use(
         'oauth2/authorization/naver',
         createProxyMiddleware({
-            target: 'https://perfume-bside.site/',  // 서버 url 설정
+            target: 'https://www.perfume-bside.site/',  // 서버 url 설정
             changeOrigin: true,
         })
     );
