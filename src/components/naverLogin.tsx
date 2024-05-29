b79d274a10d76ed48bb30bffb5838dfc3e9392ee
bbororo5 <114351464+bbororo5@users.noreply.github.com>
2024-05-29 11:32:05 +0900

    변경

--- src/components/naverLogin.tsx
+++ src/components/naverLogin.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
+import axios from 'axios';
 import naverDefault from '../assets/images/logo_green.png';
 import naverHover from '../assets/images/logo_white.png';
-import axios from 'axios';
 
 const NaverLogin = () => {
     const [isHover, setIsHover] = useState(false);
@@ -25,57 +25,24 @@ const NaverLogin = () => {
                 redirect_uri: 'https://perfume-bside.site/login/oauth2/code/naver'
             };
 
-            // 첫 번째 요청: 서버에서 302 리디렉션 주소를 받기
+            // 네이버 로그인 URL 생성
             const queryString = new URLSearchParams(params).toString();
-            const response = await axios.get(
-                `https://perfume-bside.site/oauth2/authorization/naver?${queryString}`,
-                {
-                    maxRedirects: 0 // 리디렉션을 거부
-                }
-            );
+            const naverAuthUrl = `/naver-oauth-proxy?${queryString}`;
 
-            // 두 번째 요청: 프록시를 통해 302 리디렉션 주소로 우회 요청
-            if (response.status === 302) {
-                const redirectUrl = response.headers.location;
-                if (redirectUrl) {
-                    // 프록시를 통해 네이버 로그인 창 요청
-                    const naverResponse = await axios.get(redirectUrl, {
-                        headers: {
-                            'Content-Type': 'application/x-www-form-urlencoded'
-                        },
-                        withCredentials: true
-                    });
+            // 프록시를 통해 네이버 로그인 창 요청
+            const response = await axios.get(naverAuthUrl, {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                withCredentials: true
+            });
 
-                    // 네이버 로그인 창 HTML을 브라우저에 반환
-                    document.open();
-                    document.write(naverResponse.data);
-                    document.close();
-                }
-            } else {
-                console.error('Unexpected response status:', response.status);
-            }
+            // 네이버 로그인 창 HTML을 브라우저에 반환
+            document.open();
+            document.write(response.data);
+            document.close();
         } catch (error) {
-            const err = error as any;
-            if (err.response && err.response.status === 302) {
-                // 302 응답의 location 헤더에서 URL을 가져와 리디렉션
-                const redirectUrl = err.response.headers.location;
-                if (redirectUrl) {
-                    // 프록시를 통해 네이버 로그인 창 요청
-                    const naverResponse = await axios.get(redirectUrl, {
-                        headers: {
-                            'Content-Type': 'application/x-www-form-urlencoded'
-                        },
-                        withCredentials: true
-                    });
-
-                    // 네이버 로그인 창 HTML을 브라우저에 반환
-                    document.open();
-                    document.write(naverResponse.data);
-                    document.close();
-                }
-            } else {
-                console.error('Error fetching login URL', err);
-            }
+            console.error('Error fetching login URL', error);
         }
     };
 
@@ -92,7 +59,7 @@ const NaverLogin = () => {
                         onClick={loginNaver}
                     >
                         <div className="flex flex-row justify-between w-full h-full">
-                            <img className="flex-shrink-0 mr-1" src={naverHover} alt="Naver Hover"/>
+                            <img className="flex-shrink-0 mr-1" src={naverHover} alt="Naver Hover" />
                             <p className="w-full h-full ml-1 text-center text-white text-naver-button text-naver-default">
                                 네이버 간편 로그인
                             </p>
@@ -104,7 +71,7 @@ const NaverLogin = () => {
                         onClick={loginNaver}
                     >
                         <div className="flex flex-row justify-between w-full h-full">
-                            <img className="flex-shrink-0 mr-1" src={naverDefault} alt="Naver Default"/>
+                            <img className="flex-shrink-0 mr-1" src={naverDefault} alt="Naver Default" />
                             <p className="w-full h-full ml-1 text-center text-naver-button text-naver-default">
                                 네이버 간편 로그인
                             </p>
