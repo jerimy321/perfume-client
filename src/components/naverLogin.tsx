ab42f69a6994af9e57c9b46b4f2b291f954e0ea8
bbororo5 <114351464+bbororo5@users.noreply.github.com>
2024-05-30 05:43:13 +0900

    프록시 설정 변경(프록시 거쳐서 권한 부여 서버 접속하게 함)에 따른 팝업 기능 삭제

--- src/components/naverLogin.tsx
+++ src/components/naverLogin.tsx
@@ -1,50 +1,53 @@
 import { useState } from 'react';
 import naverDefault from '../assets/images/logo_green.png';
 import naverHover from '../assets/images/logo_white.png';
+import axios from 'axios';
 
 const NaverLogin = () => {
-    const [isHover, setIsHover] = useState(false);
+  const [isHover, setIsHover] = useState(false);
+  const loginNaver = async () => {
+    try {
+      const response = await axios.get(
+        process.env.REACT_APP_API_URL + '/oauth2/authorization/naver',
+      );
+      window.location.href = response.data;
+    } catch (error) {
+      console.error('Error Posting Login');
+    }
+  };
 
-    const loginNaver = () => {
-        const oauth2Url = process.env.REACT_APP_API_URL + '/oauth2/authorization/naver';
-        window.open(oauth2Url, 'oauth2Window', 'width=800,height=600');
-    };
-
-    return (
-        <div className={'flex justify-center items-center mb-[15px]'}>
-            <div id="naverIdLogin" className="hidden"></div>
-            <div
-                onMouseEnter={() => setIsHover(true)}
-                onMouseLeave={() => setIsHover(false)}
-            >
-                {isHover ? (
-                    <div
-                        className="cursor-pointer inline-flex flex-col w-[460px] h-[94px] px-[103px] py-[27px] items-start gap-2.5 rounded-[15px] bg-naver-default border-2 border-naver-default"
-                        onClick={loginNaver}
-                    >
-                        <div className="flex flex-row justify-between w-full h-full">
-                            <img className="flex-shrink-0 mr-1" src={naverHover} alt="Naver Hover" />
-                            <p className="w-full h-full ml-1 text-center text-white text-naver-button text-naver-default">
-                                네이버 간편 로그인
-                            </p>
-                        </div>
-                    </div>
-                ) : (
-                    <div
-                        className="cursor-pointer inline-flex flex-col w-[460px] h-[94px] px-[103px] py-[27px] items-start gap-2.5 rounded-[15px] border-2 border-naver-default"
-                        onClick={loginNaver}
-                    >
-                        <div className="flex flex-row justify-between w-full h-full">
-                            <img className="flex-shrink-0 mr-1" src={naverDefault} alt="Naver Default" />
-                            <p className="w-full h-full ml-1 text-center text-naver-button text-naver-default">
-                                네이버 간편 로그인
-                            </p>
-                        </div>
-                    </div>
-                )}
+  return (
+    <div className={'flex justify-center items-center mb-[15px]'}>
+      <div id="naverIdLogin" className="hidden"></div>
+      <div
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+      >
+        {isHover ? (
+          <div
+            className="cursor-pointer inline-flex flex-col w-[460px] h-[94px] px-[103px] py-[27px] items-start gap-2.5 rounded-[15px] bg-naver-default border-2 border-naver-default"
+            onClick={loginNaver}
+          >
+            <div className="flex flex-row justify-between w-full h-full">
+              <img className="flex-shrink-0 mr-1" src={naverHover} />
+              <p className="w-full h-full ml-1 text-center text-white text-naver-button text-naver-default">
+                네이버 간편 로그인
+              </p>
             </div>
-        </div>
-    );
+          </div>
+        ) : (
+          <div className="cursor-pointer inline-flex flex-col w-[460px] h-[94px] px-[103px] py-[27px] items-start gap-2.5 rounded-[15px] border-2 border-naver-default">
+            <div className="flex flex-row justify-between w-full h-full">
+              <img className="flex-shrink-0 mr-1" src={naverDefault} />
+              <p className="w-full h-full ml-1 text-center text-naver-button text-naver-default">
+                네이버 간편 로그인
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default NaverLogin;
