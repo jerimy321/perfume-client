e0740fd8d3354d0a224c0b57fd2700f828901b49
bbororo5 <114351464+bbororo5@users.noreply.github.com>
2024-05-30 17:58:30 +0900

    http 폼 방식

--- src/components/naverLogin.tsx
+++ src/components/naverLogin.tsx
@@ -2,49 +2,41 @@ import React, { useState } from 'react';
 import naverDefault from '../assets/images/logo_green.png';
 import naverHover from '../assets/images/logo_white.png';
 
-
 const NaverLogin = () => {
     const [isHover, setIsHover] = useState(false);
-
-    const loginNaver = async () => {
-        try {
-            // 백엔드 서버의 네이버 로그인 엔드포인트로 리디렉션
-            window.location.href = "https://perfume-bside.site/oauth2/authorization/naver";
-        } catch (error) {
-            console.error('Error during login');
-        }
-    };
 
     return (
         <div className={'flex justify-center items-center mb-[15px]'}>
             <div id="naverIdLogin" className="hidden"></div>
-            <div
-                onMouseEnter={() => setIsHover(true)}
-                onMouseLeave={() => setIsHover(false)}
-            >
-                {isHover ? (
-                    <div
-                        className="cursor-pointer inline-flex flex-col w-[460px] h-[94px] px-[103px] py-[27px] items-start gap-2.5 rounded-[15px] bg-naver-default border-2 border-naver-default"
-                        onClick={loginNaver}
-                    >
-                        <div className="flex flex-row justify-between w-full h-full">
-                            <img className="flex-shrink-0 mr-1" src={naverHover} alt="Naver Login Hover" />
-                            <p className="w-full h-full ml-1 text-center text-white text-naver-button text-naver-default">
-                                네이버 간편 로그인
-                            </p>
+            <form action="https://perfume-bside.site/oauth2/authorization/naver" method="get">
+                <div
+                    onMouseEnter={() => setIsHover(true)}
+                    onMouseLeave={() => setIsHover(false)}
+                >
+                    {isHover ? (
+                        <div
+                            className="cursor-pointer inline-flex flex-col w-[460px] h-[94px] px-[103px] py-[27px] items-start gap-2.5 rounded-[15px] bg-naver-default border-2 border-naver-default"
+                            onClick={() => document.forms[0].submit()}
+                        >
+                            <div className="flex flex-row justify-between w-full h-full">
+                                <img className="flex-shrink-0 mr-1" src={naverHover} alt="Naver Login Hover" />
+                                <p className="w-full h-full ml-1 text-center text-white text-naver-button text-naver-default">
+                                    네이버 간편 로그인
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                ) : (
-                    <div className="cursor-pointer inline-flex flex-col w-[460px] h-[94px] px-[103px] py-[27px] items-start gap-2.5 rounded-[15px] border-2 border-naver-default" onClick={loginNaver}>
-                        <div className="flex flex-row justify-between w-full h-full">
-                            <img className="flex-shrink-0 mr-1" src={naverDefault} alt="Naver Login" />
-                            <p className="w-full h-full ml-1 text-center text-naver-button text-naver-default">
-                                네이버 간편 로그인
-                            </p>
+                    ) : (
+                        <div className="cursor-pointer inline-flex flex-col w-[460px] h-[94px] px-[103px] py-[27px] items-start gap-2.5 rounded-[15px] border-2 border-naver-default" onClick={() => document.forms[0].submit()}>
+                            <div className="flex flex-row justify-between w-full h-full">
+                                <img className="flex-shrink-0 mr-1" src={naverDefault} alt="Naver Login" />
+                                <p className="w-full h-full ml-1 text-center text-naver-button text-naver-default">
+                                    네이버 간편 로그인
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                )}
-            </div>
+                    )}
+                </div>
+            </form>
         </div>
     );
 };
